d55edbd5f0e02b0ab261cb25b18a941b7d670db1
Elias Norta <55353468+Elias227@users.noreply.github.com>
2024-11-04 09:51:16 +0200

    added loading screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Footer from "@/components/Footer/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
 import CustomCursor from "@/components/Cursor/Cursor";
+import Loading from "./loading";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ThemeProvider>
           <div className="container">
             {/* <CustomCursor /> */}
+            {/* <Loading /> */}
             {children}
             <Footer />
           </div>
